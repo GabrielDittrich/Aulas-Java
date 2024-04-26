47b70ea8fd5a28882867f4fc6affdf7ad2375586
GabrielDittrich <gdittrchcj@gmail.com>
2024-04-26 11:03:58 -0300

    Personagem com toString( )

---
+++ Aula14/src/Vilao.java
@@ -0,0 +1,27 @@
+public class Vilao extends Personagem {
+
+    private int nivel;
+
+    public Vilao() {
+
+    }
+
+    public Vilao(String nome, int ptsVida, int nivel) {
+        super(nome, ptsVida);
+        this.nivel = nivel;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Nivel: " + nivel;
+    }
+
+}
